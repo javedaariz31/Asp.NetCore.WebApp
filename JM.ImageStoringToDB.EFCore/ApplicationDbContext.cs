24786f2ddd88cd12e17f0122b18d1a3e19924a55
Mohammed Javed <javedamaan21@hotmail.com>
2020-09-27 18:59:51 +0100

    JM.ImageStoringToDB.EFCore Folder Checked in

---
+++ JM.ImageStoringToDB.EFCore/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using JM.ImageStoringToDB.EFCore.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JM.ImageStoringToDB.EFCore
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees{ get; set; }
+        public DbSet<Photo> Photos { get; set; }
+    }
+}
---
+++ JM.ImageStoringToDB.EFCore/Models/Photo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace JM.ImageStoringToDB.EFCore.Data.Models
+{
+    public class Photo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string PhotoName { get; set; }
+
+        public byte[] PhotoBinary { get; set; }
+
+        public string Ocassion { get; set; }
+
+    }
+}
