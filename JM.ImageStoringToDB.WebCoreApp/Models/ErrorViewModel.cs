0f2f2c344a0faab952970271247613afc419e741
Mohammed Javed <javedamaan21@hotmail.com>
2020-09-27 19:12:16 +0100

    JM.ImageStoringToDB.WebCoreApp Folder Added

---
+++ JM.ImageStoringToDB.WebCoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JM.ImageStoringToDB.WebCoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ JM.ImageStoringToDB.WebCoreApp/Models/PhotoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JM.ImageStoringToDB.WebCoreApp.Models
+{
+    public class PhotoViewModel
+    {
+        public string PhotoName { get; set; }
+
+        public IFormFile PhotoBinary { get; set; }
+    
+        public string Ocassion { get; set; }
+    }
+}
